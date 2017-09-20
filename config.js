088e954d592f048e5e2e3693c9cd50c3a1bcf1fd
Liz <lizjeukeng@gmail.com>
2017-09-20 14:12:20 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://lizjeukeng:922727579@ds115214.mlab.com:15214/assignment3', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: 'AIzaSyA7PYkKaUriQiJWuGT7MU6nrFN2rp97S6s'
+  },
+  port: 8080
+};
---
+++ coordinates.server.controller.js
@@ -0,0 +1,27 @@
+var config = require('../config/config'),
+    request = require('request');
+
+module.exports = function(req, res, next) {
+  if(req.body.address) {
+    var options = {
+      key: config.googleMaps.key,
+      address: req.body.address
+    }
+    request({
+      url: 'https://maps.googleapis.com/maps/api/geocode/json',
+      qs: options
+      }, function(error, response, body) {
+        if(error) {
+          res.status(400).send(err);
+        }
+
+        var data = JSON.parse(body);
+        if(data.results)
+          if(data.results[0])
+            req.results = data.results[0].geometry.location;
+        next();
+    });
+  } else {
+    next();
+  }
+};
